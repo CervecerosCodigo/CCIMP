0fdf8ef56791d6e90c36d21bf094ba3bef75a62b
petlys <petterlysne@hotmail.com>
2015-04-21 15:34:36 +0200

    conflict commit

--- src/model/test_klasse.cpp
+++ src/model/test_klasse.cpp
@@ -5,15 +5,11 @@
 test_klasse::test_klasse()
 {
 
-<<<<<<< HEAD
-//    processing_interface* p = new image_processing_unit();
-//    parameters* para = new parameter_using_slider();
-//    tool* blur = new tool_blur(p, para);
-=======
+
     //processing_interface* p = new image_processing_unit();
-   // parameters* para = new parameter_using_slider();
+    //parameters* para = new parameter_using_slider();
     //tool* blur = new tool_blur(p, para);
->>>>>>> Petter
+
 
 }
 
