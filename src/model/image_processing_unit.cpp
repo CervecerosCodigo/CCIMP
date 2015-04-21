d65842c5c0a2ddbaa6be9ecea951c0ad7838d303
Espen Zaal <espenbz@gmail.com>
2015-04-21 14:57:01 +0200

    Lagt til klassen for instansiering av alle verktøy

--- src/model/image_processing_unit.cpp
+++ src/model/image_processing_unit.cpp
@@ -14,7 +14,8 @@ image_processing_unit::~image_processing_unit()
 
 
 void image_processing_unit::register_tool(image_tool* t){
-//    t->execute();
+//    QImage img;
+//    t->execute(img);
 
     tools.push_back(t);
 
