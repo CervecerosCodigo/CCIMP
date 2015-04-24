3d570e262f068a992116442bc25a420ce8f701ab
petlys <petterlysne@hotmail.com>
2015-04-24 15:37:05 +0200

    Laget konverteringsmetoder fra Image til QImage og omvendt ++

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -295,7 +295,11 @@ void MainWindow::on_pushButton_2_clicked()
 }
 
 void MainWindow::changeBrightness(int i) {
-    qDebug() << i;
+    // Endre brightness
+    Image *bilde = toImage(this->imgObject);
+    bilde->gamma();
+    this->imgObject = toQImage(bilde);
+    set_image();
 }
 
 void MainWindow::on_pushButton_clicked()
@@ -304,4 +308,51 @@ void MainWindow::on_pushButton_clicked()
     b_dialog.setModal(true);
     connect(&b_dialog, SIGNAL(signalBrightnessChanged(int)), this, SLOT(changeBrightness(int)));
     b_dialog.exec();
+}
+
+Image* MainWindow::toImage(QImage* qimage)
+{
+    qDebug() << "toImage:" << qimage->width() << qimage->height();
+
+    Image *newImage = new Image(Magick::Geometry(qimage->width(), qimage->height()), Magick::ColorRGB(0.5, 0.2, 0.3));
+
+    double scale = 1 / 256.0;
+    newImage->modifyImage();
+    Magick::PixelPacket *pixels;
+    Magick::ColorRGB mgc;
+    for (int y = 0; y < qimage->height(); y++) {
+        pixels = newImage->setPixels(0, y, newImage->columns(), 1);
+        for (int x = 0; x < qimage->width(); x++) {
+            QColor pix = qimage->pixel(x, y);
+            //      *pixels++ = Magick::ColorRGB(256 * pix.red(), 256 * pix.green(), 256 * pix.blue());
+            mgc.red(scale *pix.red());
+            mgc.green(scale *pix.green());
+            mgc.blue(scale *pix.blue());
+            //      *pixels++ = Magick::ColorRGB(scale *pix.red(), scale * pix.green(), scale * pix.blue());
+            *pixels++ = mgc;
+        }
+        newImage->syncPixels();
+    }
+
+    return newImage;
+}
+
+
+QImage* MainWindow::toQImage(Image *image)
+{
+    qDebug() << "toQImage:" << image->columns() << image->rows();
+
+    QImage *newQImage = new QImage(image->columns(), image->rows(), QImage::Format_RGB32);
+    const Magick::PixelPacket *pixels;
+    Magick::ColorRGB rgb;
+    for (int y = 0; y < newQImage->height(); y++) {
+        pixels = image->getConstPixels(0, y, newQImage->width(), 1);
+        for (int x = 0; x < newQImage->width(); x++) {
+            rgb = (*(pixels + x));
+            newQImage->setPixel(x, y, QColor((int) (255 * rgb.red())
+                                             , (int) (255 * rgb.green())
+                                             , (int) (255 * rgb.blue())).rgb());
+        }
+    }
+    return newQImage;
 }
