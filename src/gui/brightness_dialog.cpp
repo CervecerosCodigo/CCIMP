ca436ea3897ac1343e4fecbbb6ad8fd5f0d4f7e1
Lukas Larsed <git@lukaslarsed.com>
2015-04-25 14:15:33 +0200

    Brightness fungerer som brightness skal fungere

--- src/gui/brightness_dialog.cpp
+++ src/gui/brightness_dialog.cpp
@@ -4,7 +4,11 @@
 brightness_dialog::brightness_dialog(QWidget *parent) : QDialog(parent), ui(new Ui::brightness_dialog)
 {
     ui->setupUi(this);
-    ui->horizontalSlider->setSliderPosition(50);
+    ui->horizontalSlider->setMinimum(-100);
+    ui->horizontalSlider->setMaximum(100);
+    ui->horizontalSlider->setSingleStep(5);
+    ui->horizontalSlider->setTickInterval(10);
+    ui->horizontalSlider->setSliderPosition(0);
     ui->horizontalSlider->setTracking(false);
 }
 
@@ -21,6 +25,6 @@ const int &brightness_dialog::get_slider_value()
 
 void brightness_dialog::on_horizontalSlider_valueChanged(int value)
 {
- qDebug() << "brigtness_slider_actionChanged" << ui->horizontalSlider->value();
+// qDebug() << "brigtness_slider_actionChanged" << ui->horizontalSlider->value();
     emit signalBrightnessChanged();
 }
