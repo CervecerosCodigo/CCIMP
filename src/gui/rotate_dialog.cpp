df25c9c8d2aab104f25ce55e43d8e9a963367fe9
Lukas Larsed <git@lukaslarsed.com>
2015-04-27 11:16:04 +0200

    Venstre rotasjon fra main window er implementert

--- src/gui/rotate_dialog.cpp
+++ src/gui/rotate_dialog.cpp
@@ -13,9 +13,21 @@ rotate_dialog::~rotate_dialog()
     delete ui;
 }
 
+void rotate_dialog::rotate_left()
+{
+    using_coordinates* param = (using_coordinates*) tool->get_param();
+    param->set_angle(-90.0);
 
+    emit slotEditFinished();
+}
 
+void rotate_dialog::rotate_right()
+{
+    using_coordinates* param = (using_coordinates*) tool->get_param();
+    param->set_angle(90.0);
 
+    emit slotEditFinished();
+}
 
 void rotate_dialog::on_pushButton_Left_clicked()
 {
