179b55da1b979b2460d60c0dbd7aaef014d6ba06
Lukas Larsed <git@lukaslarsed.com>
2015-05-01 15:04:15 +0200

    Har reparert frem og tilbake knapper

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,7 @@
 #include "test/test_lab.cpp"
 #include "img_tools/build_tools.h"
 #include "controller/controller.h"
-#include <QString>
-#include <QStyle>
-#include <QStyleFactory>
-#include <QPalette>
+#include "lib/set_look_and_feel.h"
 
 int main(int argc, char **argv)
 {
@@ -21,33 +18,8 @@ int main(int argc, char **argv)
     build_tools* build = new build_tools{control};
     w->show();
 
-    //Skriver ut hvilke qt themes som er tilgjengelige på brukerens system.
-    //Vi kan beholde denne foreløpig slik at vi finner ut hvilken theme som man har til felles
-    qDebug() << QStyleFactory::keys();
-
-
-    QApplication::setStyle(QStyleFactory::create("Fusion"));
-    QPalette darkPalette;
-    darkPalette.setColor(QPalette::Window, QColor(153, 147, 142));
-    darkPalette.setColor(QPalette::WindowText, QColor(255, 250, 246));
-    darkPalette.setColor(QPalette::Base, QColor(137, 129, 122));
-    darkPalette.setColor(QPalette::AlternateBase, QColor(137, 129, 122));
-    darkPalette.setColor(QPalette::ToolTipBase, Qt::black);
-    darkPalette.setColor(QPalette::ToolTipText, Qt::black);
-    darkPalette.setColor(QPalette::Text, QColor(255, 250, 246));
-    darkPalette.setColor(QPalette::Button, QColor(137, 129, 122));
-    darkPalette.setColor(QPalette::ButtonText, QColor(255, 250, 246));
-//    darkPalette.setColor(QPalette::BrightText, Qt::red);
-    darkPalette.setColor(QPalette::Link, QColor(175, 160, 147));
-
-    darkPalette.setColor(QPalette::Highlight, QColor(189, 169, 152));
-    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
-
-    qApp->setPalette(darkPalette);
-
-    //qApp->setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
-    //Fargetema: http://paletton.com/#uid=7000u0k2ljc6hnI3vhb50fr6wdx
-
+    set_look_and_feel laf;
+    laf.execute_fabulous_style();
 
     return a.exec();
 
